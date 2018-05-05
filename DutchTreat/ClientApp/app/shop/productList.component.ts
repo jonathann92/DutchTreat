a527b3bad17c6b9d4bb87e4d9a79f4643edf5ae2
Jonathan Nguyen <jonathann92@gmail.com>
2018-05-04 21:03:06 -0700

    adding solution

---
+++ DutchTreat/ClientApp/app/shop/productList.component.ts
@@ -0,0 +1,22 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    selector: "product-list",
+    templateUrl: "productList.component.html",
+    styleUrls: []
+})
+export class ProductList {
+    public products = [
+        {
+            title: "First Product",
+            price: 19.99
+        },
+        {
+            title: "Second Product",
+            price: 129.99
+        },
+        {
+            title: "Third Product",
+            price: 139.99
+        }];
+}
